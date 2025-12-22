7bea5f29d8e49a656f56cb15c87cc2d9fb033764
Manu Yadav <manu.yadav@ing.com>
2025-12-22 20:03:49 +0100

    first commit

---
+++ src/main/java/com/producer/controller/MessageController.java
@@ -0,0 +1,21 @@
+package com.producer.controller;
+
+import com.producer.utils.KafkaProducer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    private final KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/send")
+    public void sendMessage(@RequestBody String message) {
+        kafkaProducer.sendMessage(message);
+    }
+}
---
+++ src/main/java/com/producer/utils/KafkaProducer.java
@@ -0,0 +1,23 @@
+package com.producer.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private final String TOPIC = "test";
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send(TOPIC, message);
+        System.out.println("Message sent to topic: " + message);
+    }
+}
